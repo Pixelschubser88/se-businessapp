91270074761f5c22c5d9cc3c86658cb8bc449858
Thim <thim.loellke@gmail.com>
2017-10-27 15:33:07 +0200

    Testsuit weiter bearbeiten

---
+++ src/test/java/com/businessapp/Testsuit.java
@@ -0,0 +1,29 @@
+package com.businessapp;
+//package com.businessapp.model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+        AppTest.class,
+        IndividualCustomerTest.class })
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author thiml
+ */
+
+
+
+public class Testsuit {
+    
+}
